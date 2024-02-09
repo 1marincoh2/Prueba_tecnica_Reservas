23cab3ea837b2075bb9e51673265e23239cafcff
1marincoh2 <josealbertomarincoh1@gmail.com>
2024-02-09 17:16:20 -0500

    Prueba Finalizado

--- src/app/modal-reserve/modal-reserve.component.ts
+++ src/app/modal-reserve/modal-reserve.component.ts
@@ -1,5 +1,5 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { SharedService } from '../service/shared.service';  
+import { SharedService } from '../service/shared.service';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import {
   FormBuilder,
@@ -9,14 +9,14 @@ import {
 } from '@angular/forms';
 
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { HttpService } from '../services/http.service';;
+import { HttpService } from '../services/http.service';
 @Component({
   selector: 'app-modal-reserve',
   templateUrl: './modal-reserve.component.html',
   styleUrls: ['./modal-reserve.component.scss'],
 })
 export class ModalReserveComponent implements OnInit {
-  showModal: boolean = false;
+
   horas: string[] = [
     '7 am',
     '8 am',
@@ -33,17 +33,15 @@ export class ModalReserveComponent implements OnInit {
   ];
   date!: string;
   hora!: string;
-  reservas!:any[]
-  favoriteSeason!: string;
+  reservas!: any[];
   reserveForm = new FormGroup({
     nombre: new FormControl('', [Validators.required]),
     email: new FormControl('', [Validators.required]),
     telefono: new FormControl('', [Validators.required]),
     fecha_entrega: new FormControl('', [Validators.required]),
-    hora:new FormControl('', [Validators.required])
+    hora: new FormControl('', [Validators.required]),
   });
-  tituloAction: string = 'Nuevo Producto';
-  btnAction: string = 'Guardar';
+  btnAction: string = 'Reservar';
   constructor(
     private dialogReferencia: MatDialogRef<ModalReserveComponent>,
     private _reserveSevice: HttpService,
@@ -51,22 +49,15 @@ export class ModalReserveComponent implements OnInit {
     private sharedService: SharedService
   ) {}
 
-  openModal() {
-    this.showModal = true;
-  }
-
-  closeModal() {
-    this.showModal = false;
-  }
+  
   getReser() {
     this._reserveSevice.getReserve().subscribe((data: any) => {
       this.reservas = data;
     });
-
   }
+
   saveReserve() {
     const payloadReserve = {
-      
       cliente: {
         nombre: this.reserveForm.value.nombre,
         telefono: this.reserveForm.value.telefono,
@@ -78,22 +69,25 @@ export class ModalReserveComponent implements OnInit {
       },
     };
     this._reserveSevice.postReserve(payloadReserve).subscribe({
-      next: (data:any) => {
+      next: (data: any) => {
         this.getReser();
         this.dialogReferencia.close();
-        this.sharedService.reservaActualizada.emit(); 
+        this.sharedService.reservaActualizada.emit();
       },
-      error: (e:any) => {
-         console.log(e,'error');
+      error: (e: any) => {
+        console.log(e, 'error');
       },
     });
-    
-     }
+  }
 
-     isHoraSelected(hour: string): boolean {
-      const reservasEnFecha = this.reservas.filter(reserva => reserva.pedido.fecha_entrega === this.date);
-      return reservasEnFecha.some(reserva => reserva.pedido.hora_entrega === hour);
-    }
+  isHoraSelected(hour: string): boolean {
+    const reservasEnFecha = this.reservas.filter(
+      (reserva) => reserva.pedido.fecha_entrega === this.date
+    );
+    return reservasEnFecha.some(
+      (reserva) => reserva.pedido.hora_entrega === hour
+    );
+  }
   ngOnInit(): void {
     this.date = this.itemDate;
     this.getReser();
